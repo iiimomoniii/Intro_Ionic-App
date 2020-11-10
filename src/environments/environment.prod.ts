e8ace3ff9906114b032641de93ac07dffa3a0f54
iiimomoniii <natdanai_thaiyala@live.com>
2020-11-10 13:05:44 +0700

    add env for project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyDqnVT2s3UacqOZSUOHlxV4GJZ27ru9NRw",
+    authDomain: "ionic-firebase-crud-73922.firebaseapp.com",
+    databaseURL: "https://ionic-firebase-crud-73922.firebaseio.com",
+    projectId: "ionic-firebase-crud-73922",
+    storageBucket: "ionic-firebase-crud-73922.appspot.com",
+    messagingSenderId: "738457717379",
+    appId: "1:738457717379:web:56e79920e0913b425769b6",
+    measurementId: "G-H0QXST8SVY"
+  }
 };
